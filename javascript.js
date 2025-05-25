bf0126ca0e24fe4f8106a0d7a013e637f23e5ab7
Kelvin Phang <kelvinphc98@gmail.com>
2025-05-25 21:20:52 +0800

    Add UI for Rock-Paper-Scissors game

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ function getRandomIntInclusive (min, max) {
     const minCeiled = Math.ceil(min);
     const maxFloored = Math.floor(max);
     return Math.floor(Math.random() * (maxFloored - minCeiled +1) + minCeiled);
-}
+};
 
 function getComputerChoice () {
     const randomNumber = getRandomIntInclusive(1, 3);
@@ -16,50 +16,88 @@ function getComputerChoice () {
         case 3:
             return "scissors";
             break;
-    }
-}
-
-function getHumanChoice () {
-    const humanChoice = prompt("Enter your choice: rock, paper, or scissors").toLowerCase();
-    if (humanChoice === "rock" || humanChoice === "paper" || humanChoice === "scissors") {
-        return humanChoice;
-    } else {
-        alert("Invalid choice. Please try again.");
-        return getHumanChoice();
-    }
-}
-
-let humanScore = 0;
-let computerScore = 0;
+    };
+};
 
 function playRound (humanChoice, computerChoice) {
-    console.log("You chose " + humanChoice + ".");
-    console.log("Computer chose " + computerChoice + ".");
+    let roundResult = "";
+    
     if (humanChoice === computerChoice) {
-        return "It's a tie!";
+        roundResult = "It's a tie!";
     } else if (
         (humanChoice === "rock" && computerChoice === "scissors") ||
         (humanChoice === "paper" && computerChoice === "rock") ||
         (humanChoice === "scissors" && computerChoice === "paper")
     ) {
         humanScore++;
-        return "You win! " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + " beats " + computerChoice + ".";
+        roundResult = "You win! " + humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1) + " beats " + computerChoice + ".";
     } else {
         computerScore++;
-        return "You lose! " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1) + " beats " + humanChoice + ".";
-    }
-}
+        roundResult = "You lose! " + computerChoice.charAt(0).toUpperCase() + computerChoice.slice(1) + " beats " + humanChoice + ".";
+    };
 
-function playGame () {
-    console.log(playRound(getHumanChoice(), getComputerChoice()));
-    console.log("Current Score - You: " + humanScore + ", Computer: " + computerScore);
-    if (humanScore === 3) {
-        console.log("Victory!");
-    } else if (computerScore === 3) {
-        console.log("Defeat!");
+    results.textContent = `You chose ${humanChoice}.
+    Computer chose ${computerChoice}.
+    ${roundResult}`;
+
+    score.textContent = `Current Score - You: ${humanScore}, Computer: ${computerScore}`;
+
+    if (humanScore === 5 || computerScore === 5) {
+        endGame();
+    };
+};
+
+function endGame() {
+    document.querySelector("#rock").disabled = true;
+    document.querySelector("#paper").disabled = true;
+    document.querySelector("#scissors").disabled = true;
+
+    const endMessage = document.createElement("div");
+    endMessage.setAttribute("id", "endMessage");
+    const resetButton = document.createElement("button");
+    resetButton.setAttribute("id", "resetButton");
+
+    if (humanScore > computerScore) {
+        endMessage.textContent = "Victory!";
     } else {
-        playGame();
-    }
-}
+        endMessage.textContent = "Defeat!";
+    };
 
-playGame();
+    document.body.appendChild(endMessage);
+
+    resetButton.textContent = "Play Again";
+    resetButton.addEventListener("click", resetGame);
+
+    document.body.appendChild(resetButton);
+};
+
+function resetGame() {
+    location.reload();
+};
+
+const container = document.querySelector("#container")
+const results = document.querySelector("#results");
+const score = document.querySelector("#score");
+
+let humanScore = 0;
+let computerScore = 0;
+
+container.addEventListener("click", (e) => {
+    const target = e.target;
+    let humanChoice
+
+    switch(target.id) {
+        case "rock":
+            humanChoice = "rock";
+            break;
+        case "paper":
+            humanChoice = "paper";
+            break;
+        case "scissors":
+            humanChoice = "scissors";
+            break;
+    };
+
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+});
